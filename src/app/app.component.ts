fb65efa1c47709250dfd3151cb779529146c703d
Bernardo <bernanceri@hotmail.com>
2019-08-20 21:37:12 -0500

    version

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import * as jsPDF from 'jspdf';
+import * as html2canvas from 'html2canvas';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'books-app';
+  
+}
