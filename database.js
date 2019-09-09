44e3a031b798c8ab72442f0b69760ad133af8e75
Bernardo <bernanceri@hotmail.com>
2019-09-09 13:08:53 -0500

    Version Final servidor

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect('mongodb://localhost/encuestas', {
   useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false
